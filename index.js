2e559913d5bf51dcb24ca26ef3b1e7d96a8b6901
Marshall Brown <marshallendel@gmail.com>
2021-06-12 23:58:19 -0400

    cleaner

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ let image2 = `images/dice${randomNumber2}.png`;
 document.querySelector(".img1").setAttribute("src", image1);
 document.querySelector(".img2").setAttribute("src", image2);
 
-(randomNumber1 > randomNumber2) ? document.querySelector("h1").innerHTML = "Player 1 Wins!" : document.querySelector("h1").innerHTML = "Player 2 Wins!";
-
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
 
 //This was how I was originally going to match the images with the number lol. Talk about WET code
 // if(randomNumber1 === 1) {
