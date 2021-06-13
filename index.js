a91a987af0624954682596710eb87667789e03b4
Marshall Brown <marshallendel@gmail.com>
2021-06-12 23:34:43 -0400

    more DRY code to make the dice roll

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let randomNumber1 = Math.floor((Math.random() * 6) + 1);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-if(randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-} else if(randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-} else if(randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-} else if(randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-} else if(randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-}else {
-    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-}
+let image1 = `images/dice${randomNumber1}.png`;
+
+document.querySelector(".img1").setAttribute("src", image1);
+
+// if(randomNumber1 === 1) {
+//     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+// } else if(randomNumber1 === 2) {
+//     document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+// } else if(randomNumber1 === 3) {
+//     document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+// } else if(randomNumber1 === 4) {
+//     document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+// } else if(randomNumber1 === 5) {
+//     document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+// }else {
+//     document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+console.log(image1);
