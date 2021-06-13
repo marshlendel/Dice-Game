5363990d3e1a9661c852823898af7f629592baff
Marshall Brown <marshallendel@gmail.com>
2021-06-12 23:37:24 -0400

    applied dice roll to the other dice

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
 
 let image1 = `images/dice${randomNumber1}.png`;
+let image2 = `images/dice${randomNumber2}.png`;
+
+
 
 document.querySelector(".img1").setAttribute("src", image1);
+document.querySelector(".img2").setAttribute("src", image2);
+
 
 // if(randomNumber1 === 1) {
 //     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
