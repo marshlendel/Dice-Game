7950aebbf6bbf4fe8bd045cdde8a57d2dfc470d7
Marshall Brown <marshallendel@gmail.com>
2021-06-12 23:20:16 -0400

    dice 1 changes with random number

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 let randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
+if(randomNumber1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+} else if(randomNumber1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+} else if(randomNumber1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+} else if(randomNumber1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+} else if(randomNumber1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+}else {
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
